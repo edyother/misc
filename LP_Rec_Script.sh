203694d1af18dcc55bc1f6c889c938a763b9c2c3
Ed Yother <ed@edyother.com>
2013-03-25 15:49:25 +0000

    Update LP_Rec_Script.sh

--- LP_Rec_Script.sh
+++ LP_Rec_Script.sh
@@ -2,35 +2,36 @@
 
 # Script for recording LPs
 
+# Enter Artist Name
 clear
 echo "Artist Name? (no spaces)"
 read art
-if [ -d $art ]; then 
-	cd $art
-	echo "Album Name? (no spaces)"
-	read alb
-	mkdir $alb
-	cd $alb
-	echo "Side?"
-	read s1
-	rec $art-$alb-$s1.ogg silence 1 0.50 0.1% 1 00:10 0.1%
-	echo "Side?"
-	read s2
-	rec $art-$alb-$s2.ogg silence 1 0.50 0.1% 1 00:10 0.1%
-else
-	mkdir $art
-	cd $art
-	echo "Album Name? (no spaces)"
-	read alb
-	mkdir $alb
-	cd $alb
-	echo "Side?"
-	read s1
-	rec $art-$alb-$s1.ogg silence 1 0.50 0.1% 1 00:10 0.1%
-	echo "Side?"
-	read s2
-	rec $art-$alb-$s2.ogg silence 1 0.50 0.1% 1 00:10 0.1%
-fi
 
-sox -V3 $art-$alb-$s1.ogg $art-$alb-$s2.ogg $alb.ogg norm
+mkdir -p $art
+cd $art
+
+# Enter Album Name
+echo "Album Name? (no spaces)"
+read alb
+
+mkdir -p $alb
+cd $alb
+
+# Enter which side is first
+echo "Side?"
+read s1
+
+# Record side 1
+rec $art-$alb-$s1.ogg silence 1 0.50 0.1% 1 00:10 0.1%
+
+# Enter which side is second
+echo "Side?"
+read s2
+
+# Record side 2
+rec $art-$alb-$s2.ogg silence 1 0.50 0.1% 1 00:10 0.1%
+
+# Concatenate and normalize into one file for later editing into individual tracks
+sox -V3 $art-$alb-$s1.ogg $art-$alb-$s2.ogg $alb.ogg gain -n -1
+
 cd ../..
